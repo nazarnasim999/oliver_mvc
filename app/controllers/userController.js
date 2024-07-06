f417aa64fed838e2987c7e356afa08cb5a4c7a70
nazarnasim999 <112262130+nazarnasim999@users.noreply.github.com>
2024-07-06 07:50:40 -0700

    /////

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -1,5 +1,5 @@
 const User = require('../models/User');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const uuid = require('uuid')
 
 exports.getUsers = async (req, res) => {
